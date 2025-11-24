e9255421c8cf10deb455d9112968106630ed6e8e
gokuhayda <82773670+gokuhayda@users.noreply.github.com>
2025-11-24 10:26:10 -0300

    Delete shipping-system-kata directory

--- shipping-system-kata/contract.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol
-
-# 1. DEFINIÇÃO DE CONTRATO (Protocol é o ABC moderno do Python)
-class ShippingStrategy(Protocol):
-    def calculate(self, weight: float) -> float:
-        pass
