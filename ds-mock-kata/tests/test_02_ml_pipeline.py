b06f24a2a18671b1b81363e29c8efa328019dbf1
gokuhayda <82773670+gokuhayda@users.noreply.github.com>
2025-11-27 14:45:01 -0300

    Update test_02_ml_pipeline.py

--- ds-mock-kata/tests/test_02_ml_pipeline.py
+++ ds-mock-kata/tests/test_02_ml_pipeline.py
@@ -12,38 +12,31 @@ except ImportError:
 
 class TestMLPipeline(unittest.TestCase):
     
-    # MOCK ONDE É USADO! Não em 'sklearn.ensemble.RandomForestClassifier'
     @patch('katas.b02_ml_pipeline.model_trainer.RandomForestClassifier')
     def test_train_and_evaluate_flow(self, mock_rf_class):
         # --- ARRANGE ---
-        # 1. Configurar a Instância do Mock
-        # Quando o código fizer `model = RandomForestClassifier()`, ele recebe este mock_instance
+        # Acesso direto ao return_value (mais idiomático)
         mock_instance = mock_rf_class.return_value
-        
-        # 2. Configurar o retorno do .predict()
-        # Simulamos que o modelo previu [1, 0] (para bater com y_test e dar 100% accuracy)
         mock_instance.predict.return_value = np.array([1, 0])
         
         trainer = ModelTrainer()
-        
-        # Dados Fakes (formato numpy)
         X_train = np.array([[1, 1], [2, 2]])
         y_train = np.array([1, 0])
         X_test  = np.array([[3, 3], [4, 4]])
-        y_test  = np.array([1, 0]) # Gabarito igual à predição simulada
+        y_test  = np.array([1, 0])
         
         # --- ACT ---
         accuracy = trainer.train_and_evaluate(X_train, y_train, X_test, y_test)
         
         # --- ASSERT ---
-        # 1. O modelo foi treinado? (Verificação de Comportamento)
-        mock_instance.fit.assert_called_once_with(X_train, y_train)
+        # Verifica o construtor 
+        mock_rf_class.assert_called_once_with(n_estimators=100, random_state=42)
         
-        # 2. A predição foi feita?
+        # Verifica os métodos
+        mock_instance.fit.assert_called_once_with(X_train, y_train)
         mock_instance.predict.assert_called_once_with(X_test)
         
-        # 3. A lógica de acurácia funcionou? (Se previu [1,0] e gabarito é [1,0], acc deve ser 1.0)
+        # Verifica o resultado
         self.assertEqual(accuracy, 1.0)
         
         print("\n✅ Kata 02: ML Training isolado com sucesso!")
-                        
