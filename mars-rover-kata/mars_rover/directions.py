27172b83cd17704f467d461d9ffea57a45db909c
gokuhayda <82773670+gokuhayda@users.noreply.github.com>
2025-12-01 07:21:56 -0300

    Create directions.py

---
+++ mars-rover-kata/mars_rover/directions.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from .entities import Position
+
+class Direction(ABC):
+
+    @abstractmethod
+    def move(self, pos: Position) -> Position:
+        pass
+
+    @abstractmethod
+    def turn_left(self):
+        pass
+
+    @abstractmethod
+    def turn_right(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+
+class North(Direction):
+    def move(self, pos): return Position(pos.x, pos.y + 1)
+    def turn_left(self): return West()
+    def turn_right(self): return East()
+    def __repr__(self): return "N"
+
+
+class South(Direction):
+    def move(self, pos): return Position(pos.x, pos.y - 1)
+    def turn_left(self): return East()
+    def turn_right(self): return West()
+    def __repr__(self): return "S"
+
+
+class East(Direction):
+    def move(self, pos): return Position(pos.x + 1, pos.y)
+    def turn_left(self): return North()
+    def turn_right(self): return South()
+    def __repr__(self): return "E"
+
+
+class West(Direction):
+    def move(self, pos): return Position(pos.x - 1, pos.y)
+    def turn_left(self): return South()
+    def turn_right(self): return North()
+    def __repr__(self): return "W"
